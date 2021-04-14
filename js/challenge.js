f7d4f4a969b25af646a8b1049070bbee2af452d1
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-14 13:15:31 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,47 +1,47 @@
-const counter = document.querySelector('#counter');
-const minus = document.querySelector('#minus');
-const plus = document.querySelector('#plus');
-const pause = document.querySelector('#pause');
-const commentInput = document.querySelector('#comment-input');
-const form = document.querySelector('#comment-form');
-const commentDiv = document.querySelector('.comments');
-const heart = document.querySelector('#heart');
+// const counter = document.querySelector('#counter');
+// const minus = document.querySelector('#minus');
+// const plus = document.querySelector('#plus');
+// const pause = document.querySelector('#pause');
+// const commentInput = document.querySelector('#comment-input');
+// const form = document.querySelector('#comment-form');
+// const commentDiv = document.querySelector('.comments');
+// const heart = document.querySelector('#heart');
 
-let count = 0;
+// let count = 0;
 
-let cInterval = window.setInterval(function(){
+// let cInterval = window.setInterval(function(){
   
-    counter.innerText = ++count;
+//     counter.innerText = ++count;
   
-}, 1000);
+// }, 1000);
 
-plus.addEventListener('click', function(){
+// plus.addEventListener('click', function(){
 
-  counter.innerText = ++count;
+//   counter.innerText = ++count;
 
-});
+// });
 
-minus.addEventListener('click', function(){
+// minus.addEventListener('click', function(){
   
-  counter.innerText = --count;
+//   counter.innerText = --count;
   
-});
+// });
 
 
-pause.addEventListener('click', function(){
-  window.clearInterval(cInterval);
-  minus.disabled = true;
-  plus.disabled = true;
-  submit.disabled = true;
-  heart.disabled = true;
+// pause.addEventListener('click', function(){
+//   window.clearInterval(cInterval);
+//   minus.disabled = true;
+//   plus.disabled = true;
+//   submit.disabled = true;
+//   heart.disabled = true;
   
-});
+// });
 
-form.addEventListener('submit', function(e){
+// form.addEventListener('submit', function(e){
   
-  e.preventDefault();
-  let ul = document.createElement('ul');
-  let li = document.createElement('li');
-  let textLi = document.createTextNode(commentInput.value);
+//   e.preventDefault();
+//   let ul = document.createElement('ul');
+//   let li = document.createElement('li');
+//   let textLi = document.createTextNode(commentInput.value);
   
-});
+// });
