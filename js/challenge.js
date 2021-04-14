5a6b14340b00b87081646ba3a05053a726d795de
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-14 13:22:38 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -40,7 +40,7 @@ pause.addEventListener('click', function(){
   
 });
 
-  if(pause.innerText.value === 'resume'){
+  if(pause.innerText === 'resume'){
     pause.addEventListener('click', function(){
       // window.setInterval(cInterval);
       alert('hey')
