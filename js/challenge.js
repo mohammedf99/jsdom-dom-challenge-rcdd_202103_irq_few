ec75c01e454e8c55eff61bdd5968179146996229
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-14 13:14:16 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,47 @@
+const counter = document.querySelector('#counter');
+const minus = document.querySelector('#minus');
+const plus = document.querySelector('#plus');
+const pause = document.querySelector('#pause');
+const commentInput = document.querySelector('#comment-input');
+const form = document.querySelector('#comment-form');
+const commentDiv = document.querySelector('.comments');
+const heart = document.querySelector('#heart');
+
+let count = 0;
+
+let cInterval = window.setInterval(function(){
+  
+    counter.innerText = ++count;
+  
+}, 1000);
+
+plus.addEventListener('click', function(){
+
+  counter.innerText = ++count;
+
+});
+
+minus.addEventListener('click', function(){
+  
+  counter.innerText = --count;
+  
+});
+
+
+pause.addEventListener('click', function(){
+  window.clearInterval(cInterval);
+  minus.disabled = true;
+  plus.disabled = true;
+  submit.disabled = true;
+  heart.disabled = true;
+  
+});
+
+form.addEventListener('submit', function(e){
+  
+  e.preventDefault();
+  let ul = document.createElement('ul');
+  let li = document.createElement('li');
+  let textLi = document.createTextNode(commentInput.value);
+  
+});
