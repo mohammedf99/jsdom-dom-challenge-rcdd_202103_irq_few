b123ce6c9a4df95aba6c7d3d81ce0a351d4d3dd1
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-04-14 13:30:45 +0000

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,50 @@
+const counter = document.querySelector('#counter');
+const minus = document.querySelector('#minus');
+const plus = document.querySelector('#plus');
+const pause = document.querySelector('#pause');
+
+const form = document.querySelector('#comment-form');
+const commentDiv = document.querySelector('.comments');
+const heart = document.querySelector('#heart');
+
+form.addEventListener('submit', function(e){
+  const commentInput = document.querySelector('#comment-input');
+  e.preventDefault();
+  let ul = document.createElement('ul');
+  let li = document.createElement('li');
+  li.innerText = commentInput.value;
+  ul.append(li);
+  commentDiv.append(ul);
+});
+
+let count = 0;
+
+let cInterval = window.setInterval(function(){
+  
+    counter.innerText = ++count;
+  
+}, 1000);
+
+plus.addEventListener('click', function(){
+
+  counter.innerText = ++count;
+
+});
+
+minus.addEventListener('click', function(){
+  
+  counter.innerText = --count;
+  
+});
+
+
+pause.addEventListener('click', function(){
+  window.clearInterval(cInterval);
+  minus.disabled = true;
+  plus.disabled = true;
+  submit.disabled = true;
+  heart.disabled = true;
+  
+  pause.innerHTML = 'resume';
+  
+});
